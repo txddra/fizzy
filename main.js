7788588dc278c9b076387cd541097fa6a3d71ba5
Your Name <you@example.com>
2020-02-04 14:42:04 -0500

    finished

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 function fizzy(num) {
-if(num % 3 === 0 && num % 5 !==0){
-return 'Fizz'
+if(num % 3 === 0 && num % 5 !== 0){
+  return 'Fizz'
 }
+if(num % 5 === 0 && num % 3 !== 0){
+  return 'Buzz'
 }
+if(num % 5 === 0 && num % 3 === 0){
+  return 'FizzBuzz'
+}else{
+  return num
+}
+
+}
+
 
 module.exports = {
   fizzy,
